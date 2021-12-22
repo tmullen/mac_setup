62b76255328a87acbfaf8c101e22163ac2af8bb8
Ted Mullen <tedmullen@gmail.com>
2021-12-21 22:10:51 -0800

    Add zsh history auto-complete.

---
+++ zsh/history.zsh
@@ -0,0 +1,12 @@
+# history setup
+setopt APPEND_HISTORY
+setopt SHARE_HISTORY
+HISTFILE=$HOME/.zhistory
+SAVEHIST=1000
+HISTSIZE=999
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt EXTENDED_HISTORY
+
+# autocompletion using arrow keys (based on history)
+bindkey '\e[A' history-search-backward
+bindkey '\e[B' history-search-forward
