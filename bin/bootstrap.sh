b31b7564d993b3d08d917197529671a6548aeabd
Ted Mullen <tedmullen@gmail.com>
2022-01-21 20:10:58 -0800

    Brew install to use full path.
    Brew was not found in the path when running a clean install.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,7 +11,7 @@ packages=(
   asdf
 )
 for p in $packages; do
-  [ ! -d /opt/homebrew/Cellar/$p ] && brew install $p
+  [ ! -d /opt/homebrew/Cellar/$p ] && /opt/homebrew/bin/brew install $p
 done
 
 echo "Installing applications..."
@@ -24,9 +24,10 @@ applications=(
   visual-studio-code
 )
 for a in $applications; do
-  [ ! -d /opt/homebrew/Caskroom/$a ] && brew install $a
+  [ ! -d /opt/homebrew/Caskroom/$a ] && /opt/homebrew/bin/brew install $a
 done
 
 echo "Installing NeoVim plugin manager..."
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
