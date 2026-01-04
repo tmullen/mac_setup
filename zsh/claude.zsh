6077aba050e8244e65e9d438708bd30036395387
Ted Mullen <tedmullen@gmail.com>
2026-01-04 09:14:58 -0800

    claude configurations for aws use

--- zsh/claude.zsh
+++ zsh/claude.zsh
@@ -1,1 +1,9 @@
-alias claude="/Users/tedmullen/.claude/local/claude"
+export AWS_REGION=us-west-2
+export CLAUDE_CODE_USE_BEDROCK=1
+
+# recommended settings by anthropic
+export MAX_THINKING_TOKENS=1024
+export CLAUDE_CODE_MAX_OUTPUT_TOKENS=8192
+
+# AWS_BEARER_TOKEN_BEDROCK in ~/.secrets
+
